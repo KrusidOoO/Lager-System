f79ffbcbacc6996303dba268e1deee9de9c9d74c
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-03-05 13:05:28 +0100

    A LOT OF CHANGES JESPER
    Yeth

--- LagerSystem/Item.cs
+++ LagerSystem/Item.cs
@@ -6,9 +6,9 @@ namespace LagerSystem
 {
     class Item
     {
-          private List<string> SerialNumbers=new List<string>() { "4100", "4101", "4102", "4103", "4104", "4105" };
-        private List<string> Product = new List<string>() { "Gloves", "Trousers", "Jeans", "T-shirts", "Overalls", "Turtle neck" };
-        private List<string> Amount = new List<string>() { "10", "25", "50", "100", "250", "500" };
+        List<string> SerialNumbers=new List<string>() { "4100", "4101", "4102", "4103", "4104", "4105" };
+        List<string> Product = new List<string>() { "Gloves", "Trousers", "Jeans", "T-shirts", "Overalls", "Turtle neck" };
+        List<string> Amount = new List<string>() { "10", "25", "50", "100", "250", "500" };
 
         public List<string> GetSerial()
         {
--- LagerSystem/Program.cs
+++ LagerSystem/Program.cs
@@ -8,11 +8,12 @@ namespace LagerSystem
 {
     class Program
     {
-        Item Lists = new Item();
-
-        
         static void Main(string[] args)
         {
+            //List<string> SerialNumbers = new List<string>() { "4100", "4101", "4102", "4103", "4104", "4105" };
+            //List<string> Product = new List<string>() { "Gloves", "Trousers", "Jeans", "T-shirts", "Overalls", "Turtle neck" };
+            //List<string> Amount = new List<string>() { "10", "25", "50", "100", "250", "500" };
+            TaskHandler TH = new TaskHandler();
             bool LoginOK = false;
             int tries=0;
             string name;
@@ -53,54 +54,13 @@ namespace LagerSystem
                     //Finding a product
                     if (myChoiceOfKey.KeyChar == '1')
                     {
-                        Console.Clear();
-                        string getProduct = "";
-                        Console.WriteLine("Please input the product you want to know more of");
-                        string productSearch = Console.ReadLine();
-                        int x = .IndexOf(productSearch);
-                        Console.WriteLine();
-                        if (ProductName[x].Contains(productSearch))
-                            getProduct = ProductName[x];
-                        Console.WriteLine();
-                        int index = ProductName.IndexOf(productSearch);
-
-                        var getSerialNumber = SerialNumbers.ElementAt(index);
-                        var getAmount = Amount.ElementAt(index);
-                        Console.Clear();
-                        Console.WriteLine();
-                        Console.WriteLine("#" + getSerialNumber + " : " + char.ToUpper(getProduct[0])+getProduct.Substring(1) + "\nAmount: " + getAmount);
-                        Console.WriteLine("----------------------------------------");
-                        Console.WriteLine("Press any key to return to the main menu");
-                        Console.ReadKey();
+                        TH.Search();
                     }
 
                     //Adding a product with serial number and amount
                     if(myChoiceOfKey.KeyChar=='2')
                     {
-                            Console.Clear();
-                            Console.WriteLine();
-                            Console.WriteLine("So you wish to add a product to the site, \nfirstly enter the serial number you want to give it");
-                            string AddSerial = Console.ReadLine();
-                            if (SerialNumbers.Contains(AddSerial))
-                            {
-                                Console.WriteLine("Error! serial number already exists");
-                                Console.ReadKey();
-                            }
-                            SerialNumbers.Add(AddSerial);
-                            Console.WriteLine("Success! Now, what is the name of the product?");
-                            string AddProduct = Console.ReadLine();
-                            if(ProductName.Contains(AddProduct))
-                            {
-                                Console.WriteLine("Error! Product name already exists");
-                                Console.ReadKey();
-                            }
-                            ProductName.Add(AddProduct);
-                        Console.WriteLine("Succes! Now how many of the product are there?");
-                        string AddAmount = Console.ReadLine();
-                        Amount.Add(AddAmount);
-                        Console.Clear();
-                        Console.WriteLine("Now. Press any key to return to the main menu");
-                        Console.ReadKey();
+                        TH.Addition();
                     }
 
                     //Removing/changing a product and/or serial number and/or amount
@@ -111,29 +71,24 @@ namespace LagerSystem
                     //Getting a list of the products in stock
                     if (myChoiceOfKey.KeyChar == '4')
                     {
-                        Console.Clear();
-                        Console.WriteLine("Here is a list of our products");
-                        for (int i=0; i < ProductName.Count(); i++)
-                        {
-                            Console.WriteLine(ProductName[i]);
-                        }
-                        Console.WriteLine();
-                        Console.WriteLine("----------------------------------------");
-                        Console.WriteLine();
-                        Console.WriteLine("Press any key to return to the main menu");
-                        Console.ReadKey();
+                        //Console.Clear();
+                        //Console.WriteLine("Here is a list of our products");
+                        //for (int i=0; i < Product.Count(); i++)
+                        //{
+                        //    Console.WriteLine(Product[i]);
+                        //}
+                        //Console.WriteLine();
+                        //Console.WriteLine("----------------------------------------");
+                        //Console.WriteLine();
+                        //Console.WriteLine("Press any key to return to the main menu");
+                        //Console.ReadKey();
                     }
 
 
                 } while (myChoiceOfKey.Key != ConsoleKey.Escape);
 
             }
-        }
-        public void RetrieveLists()
-        {
-            List<string> calledSerial = Lists.GetSerial();
-            List<string> calledProduct = Lists.GetProduct();
-            List<string> calledAmount = Lists.GetAmount();
+            
         }
     }
 }
